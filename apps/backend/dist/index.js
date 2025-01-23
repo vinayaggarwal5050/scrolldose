43a7329b5119dffbe9df9a5eaa5e472328b9c421
vinay aggarwal <142632483+vinayaggarwal5050@users.noreply.github.com>
2025-01-24 03:05:27 +0530

    port-changed

--- apps/backend/dist/index.js
+++ apps/backend/dist/index.js
@@ -9,18 +9,38 @@ const channelPartner_router_1 = require("./Routers/channelPartner.router");
 const store_router_1 = require("./Routers/store.router");
 const product_router_1 = require("./Routers/product.router");
 const userRoute_1 = require("./Routers/userRoute");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const PORT = 6000;
-app.listen(PORT, () => {
+const PORT = 5000;
+app.listen(PORT, "0.0.0.0", () => {
     console.log("API server started on port: ", PORT);
 });
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "connect-src 'self' http://localhost:6000"
+//   );
+//   next();
+// });
+// const corsOptions = {
+//   origin: "http://localhost:8081",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+// app.use(cors(corsOptions));
 app.get("/api/v1", (req, res) => {
     res.json({
         msg: "hello from home page",
     });
 });
 //using Routers
+app.get("/", (req, res) => {
+    res.json({
+        msg: "welcome to homepage"
+    });
+});
 app.use("/api/v1/super-admin", superadmin_router_1.superadminRouter);
 app.use("/api/v1/channel-partner", channelPartner_router_1.channelPartnerRouter);
 app.use("/api/v1/store", store_router_1.storeRouter);
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,15 +4,32 @@ import { channelPartnerRouter } from "./Routers/channelPartner.router";
 import { storeRouter } from "./Routers/store.router";
 import { productRouter } from "./Routers/product.router";
 import { userRouter } from "./Routers/userRoute";
+import cors from "cors";
 
 const app = express();
-const PORT = 6000;
+const PORT = 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("API server started on port: ", PORT);
 });
 
 app.use(express.json());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     "connect-src 'self' http://localhost:6000"
+//   );
+//   next();
+// });
+
+// const corsOptions = {
+//   origin: "http://localhost:8081",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+// app.use(cors(corsOptions));
 
 app.get("/api/v1", (req: Request, res: Response) => {
   res.json({
@@ -21,6 +38,11 @@ app.get("/api/v1", (req: Request, res: Response) => {
 });
 
 //using Routers
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    msg: "welcome to homepage"
+  })
+})
 app.use("/api/v1/super-admin", superadminRouter);
 app.use("/api/v1/channel-partner", channelPartnerRouter);
 app.use("/api/v1/store", storeRouter);
